8c0b989acbb71b2ff4e65215b573f16ba33957f0
DanexExe <125923529+Danex-Exe@users.noreply.github.com>
2024-11-04 00:00:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ try:
                     try:
                         type_attack = ('SMS', 'CALL', 'FEEDBACK')
                         if url['info']['attack'] in type_attack:
-                            async with session.request( url['method'], url['url'], params=url.get('params'), cookies=url.get('cookies'), headers=url.get('headers'), data=url.get('data'), json=url.get('json'), timeout=20) as response:
+                            async with session.request( url['method'], url['url'], params=url.get('params'), cookies=url.get('cookies'), headers=url.get('headers'), data=url.get('data'), json=url.get('json'), timeout=60) as response:
                                 start_time = time.time()  # Начальное время
                                 end_time = start_time + attack_duration
                                 if time.time() >= end_time: return
